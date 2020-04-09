def6e9380bd8c3192bc2fea3e30bdc81595dc92b
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 19:34:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,13 @@
 def hello_t(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-  array
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
 end
 hello_t(["Tim", "Tom", "Jim"])
 # call your method here!
